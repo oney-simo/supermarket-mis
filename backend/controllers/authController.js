891b59676d4cf0c4ac488eddd2eb6d435ca0c32e
oney-simo <oneysimo77@gmail.com>
2026-07-30 15:19:09 -0800

    Fix import case for User model in authController

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,6 +1,6 @@
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
-const User = require('../models/user');
+const User = require('../models/User');
 const { logActivity } = require('../services/activityLogger');
 
 const JWT_SECRET = process.env.JWT_SECRET || 'supermarket-secret';
