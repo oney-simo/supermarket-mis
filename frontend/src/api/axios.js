aed4b3e0d740f7d3cfc87837531bf9caa4cdde18
oney-simo <oneysimo77@gmail.com>
2026-07-31 05:36:09 -0800

    Change API base URL to production server

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://supermarket-mis.onrender.com',
   headers: {
     'Content-Type': 'application/json'
   }
