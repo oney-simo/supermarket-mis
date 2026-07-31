fa90039490894d33112be173cf06a2a8edc72471
oney-simo <oneysimo77@gmail.com>
2026-07-31 05:41:52 -0800

    Update baseURL for API requests

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://supermarket-mis.onrender.com',
+  baseURL: 'https://supermarket-mis.onrender.com/api',
   headers: {
     'Content-Type': 'application/json'
   }
